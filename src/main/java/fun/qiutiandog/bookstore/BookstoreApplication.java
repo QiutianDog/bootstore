05799ceffc11e587926dfa165ece4fd6463e0e8a
QiutianDog <1019652866@qq.com>
2021-05-19 14:48:06 +0800

    init project

---
+++ src/main/java/fun/qiutiandog/bookstore/BookstoreApplication.java
@@ -0,0 +1,16 @@
+package fun.qiutiandog.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author QiutianDog
+ */
+@SpringBootApplication
+public class BookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+}
